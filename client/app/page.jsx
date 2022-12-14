260174f73c74a7627050e4f34e754e444a16535f
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-14 10:48:23 -0600

    feat: install material ui.

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { Grid, Typography } from '@mui/material';
+
 export default function Home() {
     return (
-        <div>
-            <h1>MWI Serve</h1>
-        </div>
+        <Grid container>
+            <Grid item>
+                <Typography variant="h1">MWI Serve</Typography>
+            </Grid>
+        </Grid>
     );
 }
