5f4be31e8f5b24307df6f789e18e5587e0d3b683
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-15 10:28:47 -0600

    feat: create skeleton next.js project

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,13 +1,3 @@
-'use client';
-
-import { Grid, Typography } from '@mui/material';
-
 export default function Home() {
-    return (
-        <Grid container>
-            <Grid item>
-                <Typography variant="h1">MWI Serve</Typography>
-            </Grid>
-        </Grid>
-    );
+    return <h1>MWI Serve</h1>;
 }
