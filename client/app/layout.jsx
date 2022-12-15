fdaff975facfcd87b7982c38b53eb05c733abba1
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-15 10:31:36 -0600

    feat: add MUI

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,8 +1,23 @@
+"use client";
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from '../styles/theme';
+
+import { Container } from '@mui/system';
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head />
-            <body>{children}</body>
+            <body>
+              <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Container>
+                  {children}
+                </Container>
+              </ThemeProvider>
+            </body>
         </html>
     );
 }
