41af23a045080ac44e06c613db2db4d6f7b995a8
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2022-12-16 12:20:17 -0600

    feat: create login page.

---
+++ client_new/pages/login.js
@@ -0,0 +1,88 @@
+import React, { useEffect } from 'react';
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Formik } from 'formik';
+// import loginValidators from "../../validators/loginValidators";
+const axios = require('axios');
+import Cookies from 'js-cookie';
+// import { checkAuthenticated } from '../../helpers/authenticationHelper';
+
+export default function Login() {
+    // useEffect(() => {
+    //     checkAuthenticated();
+    // });
+
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant="h3">Login</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        password: '',
+                    }}
+                    // validationSchema={loginValidators}
+                    onSubmit={(values, { setSubmitting }) => {
+                        axios.post('http://localhost:8000/api/login', values)
+                            .then((response) => {
+                                Cookies.set('token', response.data.token);
+                                setSubmitting(false);
+                                window.location.replace('/');
+                            }).catch((error) => {
+                                console.log(error);
+                                setSubmitting(false);
+                            });
+                    }}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                        isValid
+                    }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        type="text"
+                                        name="email"
+                                        label="Email"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                    /> 
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        type="password"
+                                        name="password"
+                                        label="Password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                    /> 
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button 
+                                        variant="contained" 
+                                        type="submit"
+                                        disabled={isSubmitting || !isValid}
+                                    >
+                                        Login
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    )}
+                </Formik>
+            </Grid>
+        </Grid>
+    );
+};
