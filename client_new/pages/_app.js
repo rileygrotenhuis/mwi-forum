d2c0ddc38a32e606a7d67620b557f07aee2f1fc7
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2022-12-16 11:35:54 -0600

    feat: add material ui theme provider

--- client_new/pages/_app.js
+++ client_new/pages/_app.js
@@ -1,3 +1,15 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from '../styles/theme';
+import { Container } from '@mui/system';
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container>
+                <Component {...pageProps} />
+            </Container>
+        </ThemeProvider>
+    );
 }
