22a24f77f7d21e067cbcff566bc51983258d1329
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2022-12-19 09:52:18 -0600

    feat: install recoil

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,14 +2,17 @@ import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import theme from '../styles/theme';
 import { Container } from '@mui/system';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }) {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Container>
-                <Component {...pageProps} />
-            </Container>
-        </ThemeProvider>
+        <RecoilRoot>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Container>
+                    <Component {...pageProps} />
+                </Container>
+            </ThemeProvider>
+        </RecoilRoot>
     );
 }
