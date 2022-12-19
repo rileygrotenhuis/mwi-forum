534ea7b243472302d304de12847368fd0344bc86
Riley Grotenhuis <rg@MWI-rg-P9465HHHCY.local>
2022-12-19 11:39:15 -0600

    feat: create form for creating a post

--- api/app/Models/Post.php
+++ api/app/Models/Post.php
@@ -18,7 +18,7 @@ class Post extends Model
     protected $fillable = [
         'title',
         'content',
-        'post',
+        'tag',
     ];
 
     /**
---
+++ client/validators/postValidators.js
@@ -0,0 +1,9 @@
+import * as Yup from 'yup';
+
+const postValidators = Yup.object().shape({
+    title: Yup.string().required(),
+    content: Yup.string().required(),
+    tag: Yup.string().required()
+});
+
+export default postValidators;
